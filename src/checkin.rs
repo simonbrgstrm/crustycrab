e8c37bc9982ba16ec5e221af786f57534717aebd
Simon Bergstrom <simonbrgstrm@gmail.com>
2023-11-02 16:17:29 +0100

    stuffs

--- src/checkin.rs
+++ src/checkin.rs
@@ -1,6 +1,27 @@
 extern crate chrono;
+use std::io::Read;
+use curl::easy::{Easy, List};
 
 pub fn checkin_time() {
-    // Implement the check-in logic here
-    println!("Checked in at: {:?}", chrono::Local::now());
+    let mut data = "{\"userID\": 11440, \"date\": \"2023-11-02\", \"startTime\": \"2023-11-02 07:00\", \"hours\": null, \"minutes\": null, \"activityID\": 25734, \"timeCodeID\": 41468, \"projectID\": 87916, \"comment\": \"test\", \"isInvoiced\": false}".as_bytes();
+
+    let mut easy = Easy::new();
+    easy.url("https://api2.timekeeper.se/api/v1/TimeRegistration").unwrap();
+    let mut list = List::new();
+    list.append("Authorization: Basic token==").unwrap();
+    list.append("Content-Type: application/json").unwrap();
+    easy.http_headers(list).unwrap();
+    easy.perform().unwrap();
+    easy.post(true).unwrap();
+    easy.post_field_size(data.len() as u64).unwrap();
+
+    let mut transfer = easy.transfer();
+    transfer.read_function(|buf| {
+        Ok(data.read(buf).unwrap_or(0))
+    }).unwrap();
+    transfer.perform().unwrap();
+
+
+
+
 }
--- src/checkout.rs
+++ src/checkout.rs
@@ -1,6 +1,18 @@
 extern crate chrono;
+extern crate reqwest;
 
-pub fn checkout_time() {
-    // Implement the check-in logic here
-    println!("Checked out at: {:?}", chrono::Local::now());
+pub async fn checkout_time() -> Result<(), reqwest::Error> {
+    // Specify the URL you want to send a GET request to
+    let url = "https://api2.timekeeper.se/api/v1/TimeRegistration/";
+
+    let response = reqwest::get(url).await?;
+
+    if response.status() == reqwest::StatusCode::OK {
+        let body = response.text().await?;
+        println!("Response body:\n{}", body);
+        println!("Checked in at: {:?}", chrono::Local::now());
+    } else {
+        println!("Request failed with status code: {:?}", response.status());
+    }
+    Ok(())
 }
